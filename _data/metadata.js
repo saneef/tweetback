a59eac5cc4627ce573ec4f4504addd293148c1f4
Saneef Ansari <hello@saneef.com>
2022-11-23 13:01:16 +0530

    Updates meta data

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,9 +1,9 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+  username: "saneef", // No leading @ here
+  homeLabel: "saneef.com",
+  homeUrl: "https://saneef.com/",
 };
 
-data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
+data.avatar = `https://secure.gravatar.com/avatar/7bb036b5a416f619fe23e4b6b983d4c7.jpg?size=144`;
 
 module.exports = data;
